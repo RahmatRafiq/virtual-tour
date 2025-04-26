bd440b7517908d24bbc05c5da809bb1d0a60a216
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-04-27 01:27:12 +0800

    feat: add relationships for VirtualTour and Category models

--- app/Models/VirtualTour.php
+++ app/Models/VirtualTour.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class VirtualTour extends Model
@@ -18,5 +18,9 @@ class VirtualTour extends Model
     public function spheres(): HasMany
     {
         return $this->hasMany(Sphere::class);
+    }
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
     }
 }
