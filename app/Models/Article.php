e40840c3c67457300f125cfbb296022bf0bbe642
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-05-19 19:42:25 +0800

    feat: create Article model and migration for articles table

---
+++ app/Models/Article.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Article extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'category_id',
+        'title',
+        'slug',
+        'content',
+        'tags',
+    ];
+
+    protected $casts = [
+        'tags' => 'array',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
